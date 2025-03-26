8663562b94e4e61149b6fa911b5cc5a7c4fc7421
Abdalrahman-Mohammad <abd0004@hotmail.com>
2025-03-26 07:08:30 +0200

    Add animation to navbar list

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 let state = "hidden";
 let showlist = document.getElementById("showlist");
 let list = document.getElementById("navlist");
+let inners = document.getElementsByClassName("inner-navlist");
 let nav = document.getElementById("navbar");
 list.style.visibility = "hidden";
-list.style.top=nav.offsetHeight+"px";
+list.style.top = nav.offsetHeight + "px";
 showlist.addEventListener("click", () => {
-  if(state=="hidden"){
+  if (state == "hidden") {
     list.style.visibility = "visible";
+    list.style.backdropFilter = "blur(5px)";
+    for (let item of inners) {
+      item.style.width = nav.offsetWidth + "px";
+    }
     state = "visible";
-  }else{
-    list.style.visibility = "hidden";
+  } else {
+    list.style.backdropFilter = "blur(0px)";
+    for (let item of inners) {
+      item.style.width = "0%";
+    }
+    setTimeout(() => {
+      for (let item of inners) {
+        item.style.color = "transparent";
+      }
+    }, 600);
+
+    setTimeout(() => {
+      list.style.visibility = "hidden";
+      for (let item of inners) {
+        item.style.color = "white";
+      }
+    }, 1000);
     state = "hidden";
   }
 });
 
-let inners = document.getElementsByClassName("inner-navlist");
-for(let item of inners){
+for (let item of inners) {
   item.addEventListener("click", () => {
     list.style.visibility = "hidden";
     state = "hidden";
